494cfdac9c3ff0e3c01a09b54cf51d86ca76bda0
KorigamiK <korigamik@gmail.com>
2022-02-07 15:15:25 +0530

    initial setup

---
+++ include/header.h
@@ -0,0 +1,8 @@
+#pragma once
+
+enum super
+{
+    a,
+    b,
+    c
+};
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <header.h>
+#include <iostream>
+
+#define print(x) std::cout << x << std::endl
+
+int main()
+{
+    print("Hello world");
+    print(c);
+    return 0;
+}
